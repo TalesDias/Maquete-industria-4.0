2266b82df35906bbf34f834dd623bafad28d39d2
Silva <Dtales15@gmail.com>
2020-11-20 16:52:35 -0300

    Adicionando controle do Motor da Mesa

--- controle/stepper.py
+++ controle/stepper.py
@@ -1,38 +1,64 @@
-import RPi.GPIO as GPIO, time
+import RPi.GPIO as GPIO
+from time import sleep
+import enum
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(8, GPIO.OUT) # STEP
-GPIO.setup(10, GPIO.OUT) #DIR
-GPIO.setwarnings(False)
-GPIO.output(8, True)
-
+GPIO.setup(18, GPIO.OUT) # STEP
+GPIO.setup(16, GPIO.OUT) #DIR
+''' Nao funciona por algum motivo,
+provavel problema de hardware
+GPIO.setup(15, GPIO.OUT) #MS1
+GPIO.output(15, False)
+GPIO.setup(13, GPIO.OUT) #MS2
+GPIO.output(13, False)
+'''
 GPIO.setup(11, GPIO.OUT) #MS3
 GPIO.output(11, True)
-GPIO.setup(13, GPIO.OUT) #MS2
-GPIO.output(13, True)
-GPIO.setup(15, GPIO.OUT) #MS1
-GPIO.output(15, True)
 
-#iemand anders 500 
+VELOCIDADE = 0.018
+pos_atual = None
 
-step = GPIO.PWM(8, 5000)
+class Posicoes(enum.Enum):
+    Inicial  = 0
+    Led_A    = 58
+    Sensor_A = 150
+    Led_B    = 260
+    Sensor_B = 345
 
-def SpinMotor(direcao, num_steps):
-    step.ChangeFrequency(5000)
-    GPIO.output(10, direcao)
-    while num_steps > 0:
-        step.start(1)
-        time.sleep(0.01)
-        num_steps -= 1
-    step.stop()
-    GPIO.cleanup()
-    return True
+def sign(num): return 1 if num >= 0 else -1
 
-direction_input = input('Please enter 0 or C Open or Close:')
-num_steps = int(input('Please enter the number of steps: '))
 
-if direction_input == 'C':
-    SpinMotor(False, num_steps)
-else:
-    SpinMotor(True, num_steps)
+def girar(passos, direcao):
+    if (direcao < 0):
+        GPIO.output(16, False)
+    else:
+        GPIO.output(16, True)
+    
+        
+    for i in range(passos):
+        GPIO.output(18, True)
+        sleep(VELOCIDADE)
+        GPIO.output(18, False)
+        sleep(VELOCIDADE)
 
+def girar_para(pos):
+    global pos_atual
+    delta = pos.value - pos_atual.value
+    
+    if (abs(delta) > 200):
+        delta = delta + 400*sign(-delta)
+
+    if(delta > 0):
+        girar(abs(delta), 1)
+    else:
+        girar(abs(delta), -1)
+    pos_atual = pos
+    
+
+def setup():
+    global pos_atual
+    pos_atual = Posicoes.Inicial
+    
+    
+    
+    
