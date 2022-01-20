fcea488a1998282316024b0fda296d61adb2eee0
Silva <Dtales15@gmail.com>
2022-01-19 21:50:00 -0300

    Adicionando relay para trocar o sensor rfid a ser lido

--- controle/sensor_cor.py
+++ controle/sensor_cor.py
@@ -17,10 +17,14 @@ class Cor(Enum):
     
 sensor_A = 0 #CE0
 sensor_B = 1 #CE1
+enable_B = 7
+
+G.setmode(G.BOARD)
+G.setup(enable_B, G.OUT)
+
 led_A = 32
 led_B = 36
 
-G.setmode(G.BOARD)
 G.setup(led_A, G.OUT)
 G.output(led_A, False)
 G.setup(led_B, G.OUT)
@@ -31,23 +35,28 @@ with open('mapeamento_tag_cor.json','r') as fp:
    mapeamento = json.load(fp) 
 
 
-# Todo: Verificar a necessidade de colocar um try-catch na leitura
-
 def cor(sensor):
-    leitor = SM(bus=0,device=sensor)
-    limite = 10
+    if (sensor == sensor_A):
+        G.output(enable_B, False)        
+    else:
+        G.output(enable_B, True)        
+
     ligar_led(sensor)
     sleep(2) # da a sensação de que algum trabalho foi feito 
-   
+    leitor = SM(bus=0,device=sensor)
+    limite = 10
+    
     while limite > 0:
         limite -= 1
         id = str(leitor.read_id())
         if(id in mapeamento):
             desligar_led(sensor)
+            print(Cor(mapeamento[id]))
             return Cor(mapeamento[id])
         
     
     desligar_led(sensor)    
+    print("Sensor não reconheceu a cor")
     #Caso o sensor não reconheca, retorna como padrão vermelho
     return Cor.Vermelho
 
